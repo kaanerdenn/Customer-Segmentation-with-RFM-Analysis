32e75ad99d9d126047d432db29bfb932961b1493
kaanerdenn <120827231+kaanerdenn@users.noreply.github.com>
2023-04-15 14:48:19 +0300

    Create Customer Segmentation with RFM Analysis.py

---
+++ Customer Segmentation with RFM Analysis.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import datetime as dt
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+pd.set_option('display.width',1000)
+
+df_ = pd.read_csv("flo_data_20k.csv")
+df = df_.copy()
+
+df.head(10)
+df.columns
+df.shape
+df.describe().T
+df.isnull().sum()
+df.info()
+
+
+df["order_num_total"] = df["order_num_total_ever_online"] + df["order_num_total_ever_offline"]
+df["customer_value_total"] = df["customer_value_total_ever_offline"] + df["customer_value_total_ever_online"]
+
+date_columns = df.columns[df.columns.str.contains("date")]
+df[date_columns] = df[date_columns].apply(pd.to_datetime)
+
+df.groupby("order_channel").agg({"master_id":"count",
+                                 "order_num_total":"sum",
+                                 df.sort_values("customer_value_total", ascending=False)[:10]
+                                     df.sort_values("order_num_total", ascending=False)[:10]
+                                 "customer_value_total":"sum"})
+
+def data_prep(dataframe):
+    dataframe["order_num_total"] = dataframe["order_num_total_ever_online"] + dataframe["order_num_total_ever_offline"]
+    dataframe["customer_value_total"] = dataframe["customer_value_total_ever_offline"] + dataframe["customer_value_total_ever_online"]
+    date_columns = dataframe.columns[dataframe.columns.str.contains("date")]
+    dataframe[date_columns] = dataframe[date_columns].apply(pd.to_datetime)
+    return df
+
+rfm = pd.DataFrame()
+rfm["customer_id"] = df["master_id"]
+rfm["recency"] = (analysis_date - df["last_order_date"]).astype('timedelta64[D]')
+rfm["frequency"] = df["order_num_total"]
+rfm["monetary"] = df["customer_value_total"]
+
+rfm.head()
+
+rfm["recency_score"] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
+rfm["frequency_score"] = pd.qcut(rfm['frequency'].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
+rfm["monetary_score"] = pd.qcut(rfm['monetary'], 5, labels=[1, 2, 3, 4, 5])
+
+rfm.head()
+
+rfm["RF_SCORE"] = (rfm['recency_score'].astype(str) + rfm['frequency_score'].astype(str))
+rfm["RFM_SCORE"] = (rfm['recency_score'].astype(str) + rfm['frequency_score'].astype(str) + rfm['monetary_score'].astype(str))
+rfm.head()
+
+
+seg_map = {
+    r'[1-2][1-2]': 'hibernating',
+    r'[1-2][3-4]': 'at_Risk',
+    r'[1-2]5': 'cant_loose',
+    r'3[1-2]': 'about_to_sleep',
+    r'33': 'need_attention',
+    r'[3-4][4-5]': 'loyal_customers',
+    r'41': 'promising',
+    r'51': 'new_customers',
+    r'[4-5][2-3]': 'potential_loyalists',
+    r'5[4-5]': 'champions'
+}
+
+rfm['segment'] = rfm['RF_SCORE'].replace(seg_map, regex=True)
+
+rfm.head()
+
+rfm[["segment", "recency", "frequency", "monetary"]].groupby("segment").agg(["mean", "count"])
